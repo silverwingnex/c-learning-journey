27f28f09fc18bee1e0db34a2711037741d19b33c
silverwingnex <389215717@qq.com>
2026-03-04 22:03:13 +0800

    常量编写及换行

---
+++ 入门/constant.c
@@ -0,0 +1,13 @@
+﻿// 格式控制符 %d %f %c %s
+#include <stdio.h>
+int main()
+{
+    printf("我的学校是%s,我的高考分数为%f\n","南京信息工程大学",611.0);
+    printf("我喜欢的角色名叫%s\n","satonodiamond");
+    printf("身高：%f米\n",1.58);
+    printf("体重：%s\n","无增减哟?");
+    printf("生日：%s\n","1月30日");
+    printf("三围：%s\n","B87 W54 H84");
+    printf("鞋码%dcm\n",21);
+    return 0;
+}
