0f2f52a533f1aa21d927270b19dd062012f71786
silverwingnex <389215717@qq.com>
2026-03-05 23:32:39 +0800

    变量编写

---
+++ 入门/variable/variable.c
@@ -0,0 +1,12 @@
+﻿//声明变量数据类型，先定义变量，再赋值
+#include <stdio.h>
+int main()
+{
+    int blood = 100;
+    printf("对战-80\n");
+    blood = blood - 80;
+    printf("使用技能+60\n");
+    blood = blood + 60;
+    printf("当前血量为：%d\n",blood);
+    return 0;
+}
