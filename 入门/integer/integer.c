ab093b42dd46c471c3c394a6954d6f8e31cff509
silverwingnex <389215717@qq.com>
2026-03-07 21:40:18 +0800

    整数类型

---
+++ 入门/integer/integer.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+int main()
+{
+    //short 短整型 2byte
+    short a = 32767;
+    printf("%d\n%zu\n",a,sizeof(short));
+    
+    //int 整型 4byte
+    int b = 2147483647;    
+    printf("%d\n%zu\n",b,sizeof(int));
+
+    //long 长整型 不同系统大小不同
+    long c = 2147483647L;
+    printf("%ld\n%zu\n",c,sizeof(long));
+
+    //long long 超长整型
+    long long d = 9223372036854775807LL;
+    printf("%lld\n%zu\n",d,sizeof(long long));
+
+    //无符号整型
+    unsigned int f = 4294967295;
+    printf("%u\n",f);//注意使用%u做占位符
+
+    return 0;
+}
