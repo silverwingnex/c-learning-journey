927c0cb87f1907821e71fbf2ce38f5415116df06
silverwingnex <389215717@qq.com>
2026-03-08 21:54:32 +0800

    小数类型

---
+++ CS50 problemset/week1 C/mario-less/mario.c
@@ -0,0 +1,32 @@
+//In a file called mario.c in a folder called mario-less,implement a program in C that pyramid,using hashes(#) for bricks ,as in the below:
+//        #
+//       ##
+//      ###
+//     ####
+//    #####
+//   ######
+// ########
+#include <stdio.h>
+#include <cs50.h>
+int main()
+{
+    int height;
+    do
+    {
+        height = get_int("height: \n");
+    }
+    while(height < 1 || height > 8);
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = height - i; j > 0; j--)
+        {
+            printf(" ");
+        }
+        for (int b = 0; b < i; b++)
+        {
+            printf("#");
+        }
+        printf("\n")
+    }
+    return 0
+}
---
+++ 入门/float/float.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main()
+{
+    //float 单精度小数 4byte
+    float a = 3.14F;
+    printf("%.2f\n%zu\n",a,sizeof(float));
+    //double 双精度小数 8byte
+    double b =1.78;
+    printf("%.2lf\n%zu\n",b,sizeof(double));
+    return 0;
+}
+//无法与unsigned组合,C语言小数默认double类型
