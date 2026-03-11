5d1c5705c0008957e156150c646d40db7542c280
silverwingnex <389215717@qq.com>
2026-03-11 22:37:21 +0800

    赋值，关系，逻辑，三元运算符

---
+++ 入门/09.operator(assignment relational)/operator.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+int main()
+{
+    //assignment operator 赋值运算符
+    int a = 10;
+    int b = 20;
+    a += b; //将b的值加给a
+    printf("%d %d\n",a,b);
+    //同理，-= *= /= %= 也是左右两数进行对应计算
+    
+    //relational operator 关系运算符，1为true，0为false
+    printf("%d%d%d%d%d%d\n",a == b,a != b,a > b,a < b,a >= b,a <= b);
+}
---
+++ 入门/11.ternary operator/operator.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+int main()
+{
+    //(表达式) ? 结果1 : 结果2 三元运算符
+    // 若表达式成立，取结果1，若不成立，取结果2
+    
+    //取a,b,c中的最大值
+    int a;
+    int b;
+    int c;
+    printf("请输入a,b,c的值(要求为整数)：");
+    scanf("%d %d %d",&a,&b,&c);
+    int temp;
+    int max;
+    temp = a>b ? a : b;
+    max = c>temp ? c : temp;
+    printf("最大数为%d\n",max);
+    
+    //90分及以上，赋A等 80分及以上，赋B等 70分及以上，赋C等
+    char grade = max>=90 ? 'A' :
+                 max>=80 ? 'B' :
+                 max>=70 ? 'C' : 'D';
+    printf("最终的等第为：%c\n",grade);
+    
+    return 0;
+}
