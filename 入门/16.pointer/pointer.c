4c0de0b02d0a3b38c556f8ad2aba326947303638
silverwingnex <389215717@qq.com>
2026-03-26 22:33:26 +0800

    指针传入，使函数返回多个值

--- 入门/16.pointer/pointer.c
+++ 入门/16.pointer/pointer.c
@@ -1,6 +1,10 @@
 ﻿//指针就是一个变量，存储的是另一个变量的内存地址
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 void swap(int *a, int *b);//声明函数swap，参数为两个指针，指向需要交换的变量
+void findMAXMIN(int arr[], int len, int* max, int* min);
 
 int main(){
     //指针定义格式
@@ -25,10 +29,40 @@ int main(){
     swap(pa, pb);//调用swap函数，传入指针pa和pb，交换a和b的值
     printf("交换后a,b的值分别是%d,%d\n", a, b);
 
+    //使函数返回多个值
+    int arr[10];
+    srand(time(NULL));
+    for(int i = 0; i < 10; i++){
+        arr[i] = rand() % 100 + 1;//生成1-100的随机数，存储在数组arr中
+    }
+    int len = sizeof(arr) / sizeof(arr[0]);
+    printf("生成的随机数组为：\n");
+    for(int i = 0; i<len; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    int max = arr[0];
+    int min = arr[0];
+    findMAXMIN(arr, len, &max, &min);
+    printf("数组中的最大值是%d，最小值是%d\n", max, min);
+
 }
 
 void swap(int* a, int* b){
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+
+void findMAXMIN(int arr[], int len, int* max, int* min){
+    *max = arr[0];
+    *min = arr[0];
+    for(int i = 1; i < len; i++){
+        if(arr[i] > *max){
+            *max = arr[i];
+        }
+        if(arr[i] < *min){
+            *min = arr[i];
+        }
+    }
 }
