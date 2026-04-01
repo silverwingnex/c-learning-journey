90d8b7cf5ee312fcb79295ded9bc0d8fb61073ab
silverwingnex <389215717@qq.com>
2026-04-01 23:02:42 +0800

    二维数组和指针数组

--- 入门/16.pointer/arr pointer.c
+++ 入门/16.pointer/arr pointer.c
@@ -1,4 +1,6 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
@@ -20,6 +22,51 @@ int main()
     printf("%p\n",arr+1);//步长是一个元素的大小，因为arr是指向数组首元素的指针
     printf("%p\n",&arr+1);//步长是整个数组的大小，因为&arr是整个数组的地址
 
-    return 0;
+    //二维数组arr[m][n]，m是二维数组长度,n是每一个一维数组长度
+    int arr2[3][4] = {0};
+    srand(time(NULL));
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            arr2[i][j] = rand() % 101;
+        }
+    }
+    //利用索引访问二维数组元素
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d\t",arr2[i][j]);
+        }
+        printf("\n");
+    }
+    //利用指针访问二维数组元素
+    //二维数组指针在计算中退化为指向一维数组的指针，即指向二维数组首元素的指针
+    //数据类型int[n]，n是每一个一维数组的长度,在这里即为int (*p2)[4] = arr2;
+    int (*p2)[4] = arr2;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d\t",*(*p2 + j));//p2是指向二维数组首元素一维数组的指针，*p2是一维数组的首元素地址
+        }
+        printf("\n");
+        p2++;//指向下一行
+    }
 
+    //指针数组
+    int arr31[4] = {1,2,3,4};
+    int arr32[4] = {5,6,7,8};
+    int* arr3[2] = {arr31,arr32};//指针数组，存储的是指针，即一维数组的地址
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d\t",*arr3[i] + j);//arr3[i]是指针，即一维数组的地址，arr3[i][j]是访问一维数组元素
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
