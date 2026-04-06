c0be5cb8286f4d7257d4b916485f6b4a5f70abb5
silverwingnex <389215717@qq.com>
2026-04-06 22:56:30 +0800

    字符串数组

--- 入门/17.string/string.c
+++ 入门/17.string/string.c
@@ -20,5 +20,27 @@ int main()
     //只读数据区的特点
     //数据不可修改，定义的字符串可以复用（不会重复存储相同的字符串）
 
+    //键盘录入字符串
+    char str3[100];
+    printf("请输入一个字符串：");
+    scanf("%s", str3);//不用&，因为str3是一个数组名，代表数组的首地址
+    printf("你输入的字符串是：%s\n", str3);
+    //遍历录入的字符串
+    int i = 0;
+    while (str3[i] != '\0')
+    {
+        printf("%c ", str3[i]);
+        i++;
+    }
+    printf("\n");
+
+    //字符串数组
+    char str4[3][6] = {"abc", "def", "ghi"};
+    printf("定义的字符串数组为:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s\n", str4[i]);
+    }
+
     return 0;
 }
