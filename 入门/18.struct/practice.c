9082d6c2750bdf500309d4caf19baf6f8fb3600f
silverwingnex <389215717@qq.com>
2026-04-09 23:44:04 +0800

    结构体嵌套

---
+++ 入门/18.struct/practice.c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char get_max(struct viewpoint *vps, int n);
+
+struct viewpoint
+{
+    char name;
+    int votes;
+};
+
+int main()
+{
+    //四个景点ABCD,选一个出游,80人投票，选择票数多的景点
+    //若票数相同，A优于B，B优于C，C优于D
+    //投票情况用随机数模拟
+    struct viewpoint vp[4] = { {'A', 0}, {'B', 0}, {'C', 0}, {'D', 0} };
+    srand(time(NULL));
+    //模拟80人投票
+    for (int i = 0; i < 80; i++)
+    {
+        int vote = rand() % 4; //生成0-3的随机数,对应四个景点
+        vp[vote].votes++; //对应景点票数加1
+    }
+    //统计票数最多的景点
+    //写个获取最大值的函数
+    char max_name = get_max(vp, 4);
+    printf("投票结果:\n");
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%c: %d票\n", vp[i].name, vp[i].votes);
+    }
+    printf("得票最多的景点是: %c\n", max_name);
+
+    return 0;
+}
+
+//最大值函数
+char get_max(struct viewpoint *vps, int n)
+{
+    struct viewpoint max = vps[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (vps[i].votes > max.votes)
+        {
+            max = vps[i];
+        }
+    }
+    return max.name;
+}
--- 入门/18.struct/struct.c
+++ 入门/18.struct/struct.c
@@ -1,30 +1,45 @@
 ﻿#include <stdio.h>
+#include <string.h>
+
 void modifyStudent(struct Student* pstu);
 
 //结构体是一种用户自定义的数据类型，可以包含多个不同类型的成员变量。
+struct message//结构体嵌套
+{
+    char phone[12];
+    char email[30];
+};
+
 struct Student
 {   char name[20];
     int age;
     float score;
+    struct message msg;//结构体嵌套  
 };
 
 int main()
 {
     //定义并初始化结构体变量
-    struct Student stu1 = {"Alice", 20, 85.5};
-    struct Student stu2 = {"Bob", 22, 90.0};
+    struct Student stu1;
+    strcpy(stu1.name, "Alice");
+    stu1.age = 20;
+    stu1.score = 85.5;
+    strcpy(stu1.msg.phone, "1234567890");
+    strcpy(stu1.msg.email, "alice@example.com");
+
+    struct Student stu2 = {"Bob", 22, 90.0, {"1234567890", "bob@example.com"}};
          
     //放入数组
     struct Student students[2] = {stu1, stu2};
     for (int i = 0; i < 2; i++)
     {
-        printf("Name: %s, Age: %d, Score: %.2f\n", students[i].name, students[i].age, students[i].score);
+        printf("Name: %s, Age: %d, Score: %.2f, Phone: %s, Email: %s\n", students[i].name, students[i].age, students[i].score, students[i].msg.phone, students[i].msg.email);
     }
 
     //调用函数修改结构体变量
     modifyStudent(&stu1);
     printf("修改后的学生信息:\n");
-    printf("Name: %s, Age: %d, Score: %.2f\n", stu1.name, stu1.age, stu1.score);
+    printf("Name: %s, Age: %d, Score: %.2f, Phone: %s, Email: %s\n", stu1.name, stu1.age, stu1.score, stu1.msg.phone, stu1.msg.email);
 
     return 0;
 }
@@ -33,6 +48,6 @@ int main()
 void modifyStudent(struct Student* pstu)//正常传参即可
 {
     printf("请修改Alice的学生信息:\n");
-    scanf("%s %d %f", (*pstu).name, &((*pstu).age), &((*pstu).score));//name是数组，退化为指针
+    scanf("%s %d %f %s %s", (*pstu).name, &((*pstu).age), &((*pstu).score), (*pstu).msg.phone, (*pstu).msg.email);//name是数组，退化为指针
 
 }
